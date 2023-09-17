d5f6c533c8c2079a4c87704b8f55c3311be28afa
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-17 21:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If required.enter = ()=>{
+let e=document.getElementById("d1");
+let el=document.getElementById("status");
+el.remove();
+let par=document.createElement("h1");
+let no=document.createTextNode("Entered Metaverse");
+par.appendChild(no);
+e.appendChild(par);
+}
