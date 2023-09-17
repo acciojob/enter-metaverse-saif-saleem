3eca02bd31651c99fbd03a8a19ae539eeab0d381
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-17 21:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ enter = ()=>{
 let e=document.getElementById("d1");
 let el=document.getElementById("status");
 el.remove();
+	let pr=document.createElement("p");
 let par=document.createElement("h1");
 let no=document.createTextNode("Entered Metaverse");
 par.appendChild(no);
+	pr.appendChild(par);
 e.appendChild(par);
 }
